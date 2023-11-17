2ade678e5f0e57adf9d92265be9116306eb87103
akashrathi01 <144168648+akashrathi01@users.noreply.github.com>
2023-11-17 11:04:18 -0800

    updates as of 11:04 AM 11/17/23

--- validate.py
+++ validate.py
@@ -14,34 +14,34 @@ def validate(train_data, train_labels, validation_data, validation_labels, param
 
   ## Run models
 
-  # svm_pred = svm_model(data, labels, val_data, parameters)
+  svm_pred = svm_model(data, labels, val_data, parameters)
 
-  # rf_pred = random_forest_model(data, labels, val_data, parameters)
+  rf_pred = random_forest_model(data, labels, val_data, parameters)
 
-  # hmm_pred = hmm_model(data, labels, val_data, parameters)
+  hmm_pred = hmm_model(data, labels, val_data, parameters)
 
-  # kmeans_pred = kmeans_model(data, labels, val_data, parameters)
+  kmeans_pred = kmeans_model(data, labels, val_data, parameters)
 
   # CNN
 
-  # rnn_pred = rnn_model(train_data, train_labels, validation_data, parameters)
+  rnn_pred = rnn_model(train_data, train_labels, validation_data)
 
 
   ## Compare using F2 scoring (beta > 1 gives more weight to recall)
-  # svm_f2_score = fbeta_score(y_true, svm_pred, average='weighted', beta=2)
-  # rf_f2_score = fbeta_score(y_true, rf_pred, average='weighted', beta=2)
-  # hmm_f2_score = fbeta_score(y_true, hmm_pred, average='weighted', beta=2)
-  # kmeans_f2_score = fbeta_score(y_true, kmeans_pred, average='weighted', beta=2)
-  # cnn_f2_score = fbeta_score(y_true, cnn_pred, average='weighted', beta=2)
-  # rnn_f2_score = fbeta_score(y_true, rnn_pred, average='weighted', beta=2)
+  svm_f2_score = fbeta_score(y_true, svm_pred, average='weighted', beta=2)
+  rf_f2_score = fbeta_score(y_true, rf_pred, average='weighted', beta=2)
+  hmm_f2_score = fbeta_score(y_true, hmm_pred, average='weighted', beta=2)
+  kmeans_f2_score = fbeta_score(y_true, kmeans_pred, average='weighted', beta=2)
+  cnn_f2_score = fbeta_score(y_true, cnn_pred, average='weighted', beta=2)
+  rnn_f2_score = fbeta_score(y_true, rnn_pred, average='weighted', beta=2)
 
   # Compare using confusion matrices
-  #svm_cm = confusion_matrix(y_true, svm_pred)
-  #rf_cm = confusion_matrix(y_true, rf_pred)
-  #hmm_cm = confusion_matrix(y_true, hmm_pred)
-  #kmeans_cm = confusion_matrix(y_true, kmeans_pred)
-  #cnn_cm = confusion_matrix(y_true, cnn_pred)
-  #rnn_cm = confusion_matrix(y_true, rnn_pred)
+  svm_cm = confusion_matrix(y_true, svm_pred)
+  rf_cm = confusion_matrix(y_true, rf_pred)
+  hmm_cm = confusion_matrix(y_true, hmm_pred)
+  kmeans_cm = confusion_matrix(y_true, kmeans_pred)
+  cnn_cm = confusion_matrix(y_true, cnn_pred)
+  rnn_cm = confusion_matrix(y_true, rnn_pred)
 
   #Compare using ROC curves
   #svm_roc = roc_auc(y_true, svm_pred)
