470232175525170593c989dc4c8f44a46ed81673
Katarina Pejcinovic <67714883+Katarina-Pejcinovic@users.noreply.github.com>
2023-11-28 13:39:10 -0800

    cnn return model instance, predictions, and probabilities

--- deep_learning/cnn.py
+++ deep_learning/cnn.py
@@ -3,8 +3,10 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
 
-def run_CNN(train_data, train_labels, test_data):
+def run_CNN(train_data, train_labels, test_data, test_label):
     # CNN architecture
     class SimpleCNN(nn.Module):
         def __init__(self):
@@ -12,13 +14,13 @@ def run_CNN(train_data, train_labels, test_data):
             self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
             self.relu = nn.ReLU()
             self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-            self.fc1 = nn.Linear(16 * 64 * 64, 2)  # Adjust the input size based on your data
+            self.fc1 = nn.Linear(16 * 64 * 64, 2)  # Adjust the input size later
 
         def forward(self, x):
             x = self.conv1(x)
             x = self.relu(x)
             x = self.pool(x)
-            x = x.view(-1, 16 * 64 * 64)  # Adjust the size based on your data
+            x = x.view(-1, 16 * 64 * 64)  # Adjust the size later
             x = self.fc1(x)
             return x
 
@@ -61,14 +63,14 @@ def run_CNN(train_data, train_labels, test_data):
             loss.backward()
             optimizer.step()
 
-        print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item()}")
+        #print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item()}")
 
     # Save the trained model
     torch.save(model.state_dict(), 'simple_cnn_model.pth')
 
     # Instantiate the model
     model_instance = SimpleCNN()
-    print("Model successfully created")
+    #print("Model successfully created")
 
     # Create a DataLoader for testing
     test_dataset = CustomDataset(test_data, np.zeros(len(test_data)))  # Labels are not used during testing
@@ -76,7 +78,8 @@ def run_CNN(train_data, train_labels, test_data):
 
     # Make predictions
     predictions = []
-    model.eval()  # Set the model to evaluation mode
+    model.eval()
+
     with torch.no_grad():
         for inputs, _ in test_dataloader:
             inputs = inputs.to(device)
@@ -84,7 +87,17 @@ def run_CNN(train_data, train_labels, test_data):
             _, predicted = torch.max(outputs, 1)
             predictions.extend(predicted.cpu().numpy())
 
-    return model_instance, predictions
+    #get probabilities instead of predicitions 
+    class_prob = torch.softmax(outputs, dim=1)
+
+    # get most probable class and its probability
+    class_prob = torch.max(class_prob, dim=1)
+
+    #convert to numpy 
+    prob = class_prob.values.numpy()
+ 
+    
+    return model_instance, predictions, prob
 
 
 
--- deep_learning/test_cnn.py
+++ deep_learning/test_cnn.py
@@ -4,8 +4,8 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
-from sklearn.metrics import roc_curve
-import matplotlib as plt
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
 from cnn import *
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 
@@ -22,8 +22,9 @@ train_labels = np.random.randint(0, 2, size=(100,))
 test_data = np.random.randn(20, 3, 128, 128)
 test_labels = np.random.randint(0, 2, size=(20))
 
-model_instance, predictions = run_CNN(train_data, train_labels, test_data)
-print("test_predictions", predictions)
+model_instance, predictions, output = run_CNN(train_data, train_labels, test_data, test_labels)
+print(output)
+
 
 '''evaluate the effectiveness of the model'''
 def evaluate_model(true_labels, predicted_labels):
@@ -46,30 +47,21 @@ def evaluate_model(true_labels, predicted_labels):
     print("Confusion Matrix:")
     print(cm)
 
+    # Calculate ROC curve
+    fpr, tpr, thresholds = roc_curve(test_labels, output)
+
+    # Calculate AUC
+    roc_auc = auc(fpr, tpr)
+    plt.plot(fpr, tpr)
+    plt.xlabel('False positive rate')
+    plt.ylabel('True positive rate')
+    plt.title('ROC curve')
+    plt.legend()
+    plt.show()
+    print("test_predictions", predictions)
+
+
 # Call the evaluation function
-print("test labels", test_labels, "\npredictions", len(predictions))
 evaluate_model(test_labels, predictions)
 
 
-
-# Example usage:
-# predictions = run_CNN(train_data, train_labels, test_data, test_labels)
-
-# # Run the CNN and get predictions
-# all_labels, all_predictions = run_CNN(data, labels, validation_data, validation_labels)
-
-# # Calculate ROC-AUC
-# fpr, tpr, thresholds = roc_curve(all_labels, all_predictions)
-# roc_auc = auc(fpr, tpr)
-
-# # Plot ROC curve
-# plt.figure()
-# plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = {:.2f})'.format(roc_auc))
-# plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-# plt.xlim([0.0, 1.0])
-# plt.ylim([0.0, 1.05])
-# plt.xlabel('False Positive Rate')
-# plt.ylabel('True Positive Rate')
-# plt.title('Receiver Operating Characteristic (ROC) Curve')
-# plt.legend(loc='lower right')
-# plt.show()
