84480c1cf26058b2f1eb1b838dd586964abacc47
soulaimane-altum <94147713+sbentaleb@users.noreply.github.com>
2024-01-31 00:36:27 -0800

    cut_segments and test_cut_segments now cut 5-minute segments instead of 10

--- feature_selection/cut_segments.py
+++ feature_selection/cut_segments.py
@@ -6,11 +6,11 @@ def cut_segments(data_list, label_list, patientID_list):
 # contains x channels and y data samples per channel, with sampling rate 250 Hz
 # - a list 'label_list' of 4 1D numpy arrays where each 1D numpy array of size (n,) has a label for each 2D numpy array in data_list
 # - a list 'patientID_list' of 4 1D numpy arrays where each 1D numpy array of size (n,) has the patient ID for each 2D numpy array in data_list
-# The function cuts segments of 10 minutes (150,000 samples) for all channels from each 2D np array
+# The function cuts segments of 5 minutes (75,000 samples) for all channels from each 2D np array
 # Outputs:
-# - a list 'called result_4d' of 4 3D np arrays, where each 3D np array is of size (s, x, 150000) where s is the number of 10-minute segments
-# - a list 'label_result' of 4 1D numpy arrays where each 1D numpy array of size (s,) has the label for each 10-minute segment in the 3D np arrays in result_4d
-# - a list 'patientID_result' of 4 1D numpy arrays where each 1D numpy array of size (s,) has the patientID for each 10-minute segment in the 3D np arrays in result_4d
+# - a list 'called result_4d' of 4 3D np arrays, where each 3D np array is of size (s, x, 75000) where s is the number of 5-minute segments
+# - a list 'label_result' of 4 1D numpy arrays where each 1D numpy array of size (s,) has the label for each 5-minute segment in the 3D np arrays in result_4d
+# - a list 'patientID_result' of 4 1D numpy arrays where each 1D numpy array of size (s,) has the patientID for each 5-minute segment in the 3D np arrays in result_4d
 
     result_4d = []
     label_result = []
@@ -23,10 +23,10 @@ def cut_segments(data_list, label_list, patientID_list):
 
         for i, array in enumerate(input_arrays):
             num_channels, signal_length = array.shape
-            num_segments_array = signal_length // 150000
+            num_segments_array = signal_length // 75000
 
             if num_segments_array >= 1:
-                segments_array = np.array_split(array[:, :num_segments_array * 150000], num_segments_array, axis=1)
+                segments_array = np.array_split(array[:, :num_segments_array * 75000], num_segments_array, axis=1)
                 segments.extend(segments_array)
                 labels.extend(np.full(num_segments_array, label_list[idx][i]))
                     
